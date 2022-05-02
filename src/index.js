3e13b93ec69ade49ddefbd76c3ef526826b79a8c
GautameePatel <99380053+GautameePatel@users.noreply.github.com>
2022-05-02 17:28:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
 
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-const name = "Gautami";
+const fName = "Gautami";
+const lName = "Patel";
 const Age = "25";
 ReactDOM.render(
   <div>
-    <h1>Hello {name}</h1> <p>My Age Is {Age} </p>
+    <h1>
+      Hello {fName} {lName}
+    </h1>
+    <h2>Hello {fName + " " + lName}</h2>
+    <h3>Hello {`${fName} ${lName}`}</h3>
+    <p>My Age Is {Age} </p>
   </div>,
   document.getElementById("root")
 );
